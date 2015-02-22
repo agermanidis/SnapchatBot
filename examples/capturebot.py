599026cc38c0f2b625121534faeb3b03f23a5001
Ethan Blackburn <ethan@ethan.coffee>
2015-02-21 22:28:52 -0600

    added example for saving snaps

---
+++ examples/capturebot.py
@@ -0,0 +1,28 @@
+from argparse import ArgumentParser
+import time
+
+from snapchat_bots import SnapchatBot
+
+
+class CaptureBot(SnapchatBot):
+    def on_snap(self, sender, snap):
+        self.send_snap([sender], snap)
+    
+    def listen(self, timeout=15):
+        while True:
+            self.log("Querying for new snaps...")
+            snaps = self.get_snaps(capture_snaps=True)
+            for snap in snaps:
+                self.on_snap(snap.sender, snap)
+
+            time.sleep(timeout)
+
+if __name__ == '__main__':
+    parser = ArgumentParser("Capture Bot")
+    parser.add_argument('-u', '--username', required=True, type=str, help="Username of the account to run the bot on")
+    parser.add_argument('-p', '--password', required=True, type=str, help="Password of the account to run the bot on")
+
+    args = parser.parse_args()
+
+    bot = CaptureBot(args.username, args.password)
+    bot.listen(timeout=60)
